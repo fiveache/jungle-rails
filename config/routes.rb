9f022406f856131c01182cb1f846b0859fa5b68e
David Lacho <david.lacho@gmail.com>
2018-12-07 22:47:05 +0000

    creates reviews as a nested resource of products

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
+
+  namespace :products do
+    resources :reviews, only: [:create]
+  end
+
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
