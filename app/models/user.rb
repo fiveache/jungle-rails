32605572e45b91fcd4d8e13f9891acf27350db2c
David Lacho <david.lacho@gmail.com>
2018-12-08 03:18:49 +0000

    styles the signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true
+
 end
