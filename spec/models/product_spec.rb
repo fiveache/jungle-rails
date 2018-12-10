4dfa2961340c31273974939c021c07e8ba36e0d7
David Lacho <david.lacho@gmail.com>
2018-12-10 19:54:25 +0000

    sets up tests for product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  before :each do
+
+    @category = Category.find_or_create_by! name: 'Apparel'
+
+    @product = Product.new(
+      name: 'Item',
+      price: 100,
+      quantity: 10,
+      category: @category
+    )
+  end
+
+  describe 'Validations' do
+    it 'is not valid without a name' do
+      @product.name = nil
+      expect(@product).not_to be_valid
+    end
+
+    it 'is not valid without a price' do
+      @product.price_cents = nil
+      expect(@product).not_to be_valid
+    end
+
+    it 'is not valid without a quantity' do
+      @product.quantity = nil
+      expect(@product).not_to be_valid
+    end
+
+    it 'is not valid without a category' do
+      @product.category = nil
+      expect(@product).not_to be_valid
+    end
+
+    it 'is valid with a name, price, quantity, and category' do
+      expect(@product).to be_valid
+    end
+  end
+end
