e0dd34e469c03e56d49836a6cd81f0ee92a4a259
David Lacho <david.lacho@gmail.com>
2018-12-11 21:05:39 +0000

    adds a new feature spec that will test that users can click the 'Add to Cart' button for a product on the home page and in doing so their cart increases by one

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,22 +1,29 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature , js: true do
+RSpec.feature 'AddToCarts', type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
 
-    # SETUP
-    before :each do
-      @category = Category.create! name: 'Apparel'
-
-      10.times do |n|
-        @category.products.create!(
-          name:  Faker::Hipster.sentence(3),
-          description: Faker::Hipster.paragraph(4),
-          image: open_asset('apparel1.jpg'),
-          quantity: 10,
-          price: 64.99
-        )
-      end
+    10.times do |_n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
     end
+  end
 
-
-
+  scenario 'users can click the Add to Cart button for a product on the home page and in doing so their cart increases by one' do
+    visit '/'
+    within first('article.product') do
+      first('button').click
+    end
+    within ('nav') do
+      expect(page).to have_content 'My Cart (1)'
+    end
+    save_screenshot 'cart_has_1.png'
+  end
 end
